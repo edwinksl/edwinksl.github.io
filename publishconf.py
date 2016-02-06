87cc3ef6d589996b6ec30b3d314c3ecc25ade7ca
Edwin Khoo <edwinksl@gmail.com>
2016-02-06 13:05:55 -0500

    Update site URL

--- publishconf.py
+++ publishconf.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://edwinksl.github.io'
+SITEURL = 'http://edwinksl.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
